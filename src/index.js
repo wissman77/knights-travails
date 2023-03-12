d8b01ef80179d20bd360f3749b49ff1b117a172c
Wissam Mansour <wissman77@gmail.com>
2023-03-12 09:48:33 +0200

    add buildBoard to build a chess board ui based on the produceBoard function

--- src/index.js
+++ src/index.js
@@ -4,3 +4,23 @@ import {
   knightTravails,
   produceBoard,
 } from './modules/knight-travails';
+
+(() => {
+  const boardUI = document.querySelector('.chess-board');
+  const board = produceBoard(8, 8);
+
+  const buildBoard = (theBoard, boardDiv) => {
+    for (let i = 0; i < 8; i += 1) {
+      for (let j = 0; j < 8; j += 1) {
+        const node = theBoard[i][j];
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.setAttribute('data-x', node.x);
+        div.setAttribute('data-y', node.y);
+        boardDiv.appendChild(div);
+      }
+    }
+  };
+
+  buildBoard(board, boardUI);
+})();
